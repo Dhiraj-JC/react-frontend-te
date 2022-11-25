b80aebba21d30d40caee399fc902623bf7037689
Dhiraj-JC <dhiraj@trainitsolutions.com>
2022-11-25 20:27:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchUsers, setSearchUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadDataDisabled, setloadDataDisabled] = useState(false);
+  const [searchUserName, setSearchUserName] = useState('');
+
+  function getAllUsers() {
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => {
+        setLoading(false);
+        setloadDataDisabled(true);
+        setUsers(json);
+        setSearchUsers(json);
+      });
+  }
+
+  function filterUserByName() {
+    const searchedUsers = searchUsers.filter((user) =>
+      user.name.toLowerCase().includes(searchUserName.toLowerCase())
+    );
+    setUsers(searchedUsers);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type='text'
+        placeholder='Search username'
+        id="searchInput"
+        data-testid="searchInput"
+        value={searchUserName}
+        onChange={(event) => setSearchUserName(event.target.value)}
+        onBlur={(event) => setSearchUserName(event.target.value)}
+      />
+
+<button onClick={filterUserByName}
+      >Search User</button>
+
+      <button onClick={getAllUsers}
+      disabled={loadDataDisabled}>Load data</button>
+
+
+      {loading && <h1>Loading .....</h1>}
+
+      <label data-testid="resultfound">{users.length} results found</label>
+      {users &&
+        users.map((user) => (
+          <div >
+            <hr />
+            <h1>Users Details</h1>
+            <h1>{user.name}</h1>
+            <h2>{user.email}</h2>
+            <p>{user.zipcode}</p>
+            <hr />
+          </div>
+        ))}
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders page with all the controls rendered', () => {
+  render(<App />);
+
+  expect(screen.getByTestId('searchInput')).toBeInTheDocument();
+  const loadDataButton = screen.getByRole('button', { name: 'Load data' });
+  expect(loadDataButton).toBeInTheDocument();
+  expect(loadDataButton).not.toBeDisabled();
+});
+
+test('Click load Data button and disable the button', async () => {
+  render(<App />);
+
+  const loadDataButton = screen.getByRole('button', { name: 'Load data' });
+  fireEvent.click(loadDataButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Loading .....'));
+  expect(loadDataButton).toBeDisabled();
+});
+
+test('Click load Data button and disable the button and check the result count', async () => {
+  render(<App />);
+
+  const loadDataButton = screen.getByRole('button', { name: 'Load data' });
+  fireEvent.click(loadDataButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Loading .....'));
+  expect(loadDataButton).toBeDisabled();
+
+  const users = await screen.findAllByText('Users Details');
+  expect(users).toHaveLength(10);
+});
+
+test('Click load Data button and search specific user', async () => {
+  render(<App />);
+
+  const loadDataButton = screen.getByRole('button', { name: 'Load data' });
+  fireEvent.click(loadDataButton);
+  await waitForElementToBeRemoved(() => screen.queryByText('Loading .....'));
+  expect(loadDataButton).toBeDisabled();
+
+  const searchInputElement = screen.getByTestId('searchInput');
+  expect(searchInputElement).toBeInTheDocument();
+
+  fireEvent.change(searchInputElement, { target: { value: 'le' } });
+
+  const searchButton = screen.getByRole('button', { name: 'Search User' });
+  fireEvent.click(searchButton);
+
+  expect(await screen.getByText(/4 results found/i)).toBeInTheDocument();
 });
